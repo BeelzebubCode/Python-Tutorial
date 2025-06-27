20a8d1012e80a152ca4bca458efbcb225036e369
BeelzebubCode <pongrock1234@gmail.com>
2025-06-27 21:59:44 +0700

    comment EX 47

--- 66310837/Algorithm/Medium_Ex_47.py
+++ 66310837/Algorithm/Medium_Ex_47.py
@@ -1,3 +1,16 @@
 words = sorted([input() for _ in range(int(input()))])
 
+"""
+Full Code 
+
+n = int(input())
+words = []
+
+for i in range(n):
+    words.append(i)
+
+words = sorted(words)
+
+"""
+
 print("\n".join(words))
